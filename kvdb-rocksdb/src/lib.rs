4cf0d2c6c6bcfa895d370ac63c80ef4206f920ba
Rong Ma <160457111+rongma7@users.noreply.github.com>
2025-03-27 10:32:04 +0800

    Use suggested BlockBasedOptions for partitioned index filters

--- kvdb-rocksdb/src/lib.rs
+++ kvdb-rocksdb/src/lib.rs
@@ -17,7 +17,7 @@ use std::{
 };
 
 use rocksdb::{
-	BlockBasedOptions, ColumnFamily, ColumnFamilyDescriptor, Options, ReadOptions, WriteBatch, WriteOptions, DB,
+	BlockBasedIndexType, BlockBasedOptions, ColumnFamily, ColumnFamilyDescriptor, Options, ReadOptions, WriteBatch, WriteOptions, DB
 };
 
 use kvdb::{DBKeyValue, DBOp, DBTransaction, DBValue, KeyValueDB};
@@ -312,7 +312,8 @@ fn generate_block_based_options(config: &DatabaseConfig) -> io::Result<BlockBase
 	// https://github.com/facebook/rocksdb/wiki/Setup-Options-and-Basic-Tuning#block-cache-size
 	let cache_size = config.memory_budget() / 3;
 	if cache_size == 0 {
-		block_opts.disable_cache()
+		panic!();
+		// block_opts.disable_cache()
 	} else {
 		let cache = rocksdb::Cache::new_lru_cache(cache_size).map_err(other_io_err)?;
 		block_opts.set_block_cache(&cache);
@@ -322,7 +323,17 @@ fn generate_block_based_options(config: &DatabaseConfig) -> io::Result<BlockBase
 		// Don't evict L0 filter/index blocks from the cache
 		block_opts.set_pin_l0_filter_and_index_blocks_in_cache(true);
 	}
-	block_opts.set_bloom_filter(10.0, true);
+	// block_opts.set_bloom_filter(10.0, true);
+
+	block_opts.set_bloom_filter(10.0, false); // NewBloomFilterPolicy(BITS, false)
+    block_opts.set_index_type(BlockBasedIndexType::TwoLevelIndexSearch);
+    block_opts.set_partition_filters(true);
+    block_opts.set_metadata_block_size(4096);
+    block_opts.set_cache_index_and_filter_blocks(true);
+    block_opts.set_pin_top_level_index_and_filter(true);
+    // block_opts.set_cache_index_and_filter_blocks_with_high_priority(true);
+    block_opts.set_pin_l0_filter_and_index_blocks_in_cache(true);
+    // block cache size: if you used to store the filter/index into heap, do not forget to increase the block cache size with the amount of memory that you are saving from the heap.
 
 	Ok(block_opts)
 }
